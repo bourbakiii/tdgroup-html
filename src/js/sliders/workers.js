1e864926afeaef30db0d15732d7a6866914da1f3
bourbakiii <gabisov03912@gmail.com>
2022-11-03 19:52:21 +0300

    Control point

--- src/js/sliders/workers.js
+++ src/js/sliders/workers.js
@@ -1,19 +1,46 @@
 import {Swiper} from 'swiper';
+import {gsap} from 'gsap';
 
-const createSwiper = () => new Swiper(".workers-slider-screen__slider", {
-    slidesPerView: 1,
-    breakpoints: {
-        '992': {
-            slidesPerView: 'auto'
-        }
-    }
-});
-let swiper = createSwiper();
+let swiper = null;
+const createSwiper = () => {
+    swiper = new Swiper(".workers-slider-screen__slider", {
+        slidesPerView: 1,
+        breakpoints: {
+            '992': {
+                slidesPerView: 'auto'
+            }
+        },
+        init: false
+    });
+    swiper.on('afterInit', ({activeIndex: current_slide_index}) => animateActiveSlide(current_slide_index + 1));
+    swiper.on('slideChange', ({activeIndex: current_slide_index, previousIndex: previous_slide_index}) => {
+        animatePreviousSlide(previous_slide_index + 1);
+        animateActiveSlide(current_slide_index + 1);
+    });
+    swiper.init();
+}
 
+function animateActiveSlide(index) {
+
+}
+
+function animatePreviousSlide(index) {
+
+}
+
+
+createSwiper();
+
+let timer = null;
 window.onresize = () => {
-    swiper.destroy(true, true);
-    const elementDOM = document.querySelector(".workers-slider-screen__slider");
-    elementDOM.remove();
-    document.querySelector('.workers-slider-screen').append(elementDOM);
-    swiper = createSwiper();
+    if (timer != null) return;
+    timer = setTimeout(() => {
+        clearTimeout(timer);
+        timer = null;
+        swiper.destroy(true, true);
+        const elementDOM = document.querySelector(".workers-slider-screen__slider");
+        elementDOM.remove();
+        document.querySelector('.workers-slider-screen').append(elementDOM);
+        createSwiper();
+    }, 1000)
 }
