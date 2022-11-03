ed332577e6f215a87a7b19e7de23ceb23f8f3962
bourbakiii <gabisov03912@gmail.com>
2022-11-03 17:56:07 +0300

    First version of cases swiper

--- src/js/sliders/cases.js
+++ src/js/sliders/cases.js
@@ -1,20 +1,67 @@
 import {Swiper} from 'swiper';
+import {gsap} from 'gsap';
 
-const createSwiper = () => new Swiper(".cases-slider-screen__slider", {
-    slidesPerView: 1,
-    breakpoints: {
-        '992': {
-            slidesPerView: 'auto'
+let swiper = null;
+const createSwiper = () => {
+    swiper = new Swiper(".cases-slider-screen__slider", {
+        slidesPerView: 1,
+        breakpoints: {
+            '992': {
+                slidesPerView: 'auto'
+            }
+        },
+        init: false
+    });
+    swiper.on('afterInit', ({activeIndex: current_slide_index}) => animateActiveSlide(current_slide_index + 1));
+    // swiper.on('beforeSlideChangeStart', ({activeIndex: current_slide_index}) => animatePreviousSlide(current_slide_index + 1));
+
+    swiper.on('slideChange', ({activeIndex: current_slide_index, previousIndex: previous_slide_index}) => {
+        animatePreviousSlide(previous_slide_index+1);
+        animateActiveSlide(current_slide_index + 1);
+    });
+    swiper.init();
+}
+
+function animateActiveSlide(index) {
+    console.log('Active, ', index);
+
+    const timeline = gsap.timeline();
+
+    timeline.to(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`, {
+        opacity: 0, duration: 0.2, onComplete: () => {
+            document.querySelector(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`).classList.remove('heading_5')
+            document.querySelector(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`).classList.add('heading_4');
         }
-    }
-});
+    });
+    timeline.to(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`, {
+        delay: .8,
+        opacity: 1, duration: 0.2,
 
-let swiper = createSwiper();
+    });
+}
+
+function animatePreviousSlide(index) {
+    console.log('Previous, ', index);
+    const timeline = gsap.timeline();
+    timeline.to(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`, {
+        opacity: 0, duration: 0.2, onComplete: () => {
+            document.querySelector(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`).classList.remove('heading_4');
+            document.querySelector(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`).classList.add('heading_5')
+        }
+    });
+    timeline.to(`.cases-slider-screen__slider__item:nth-child(${index}) .cases-slider-screen__slider__item__title`, {
+        delay: 0.8,
+        opacity: 1, duration: 0.2,
+    });
+}
+
+
+createSwiper();
 
 window.onresize = () => {
     swiper.destroy(true, true);
     const elementDOM = document.querySelector(".workers-slider-screen__slider");
     elementDOM.remove();
     document.querySelector('.workers-slider-screen').append(elementDOM);
-    swiper = createSwiper();
+    createSwiper();
 }
