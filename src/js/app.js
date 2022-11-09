39c1b769b759e63fc32a443293ffbc9589d98dc4
bourbakiii <gabisov03912@gmail.com>
2022-11-09 16:04:14 +0300

    Post modakl

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,11 @@ import './sliders/_cases.js'
 import './sliders/_cases-detail.js'
 
 document.querySelector('.open-form-modal-button').onclick = () => document.querySelector('.form-modal__wrapper').classList.toggle('show');
+document.querySelector('.open-post-modal-button').onclick = () => document.querySelector('.post-modal__wrapper').classList.toggle('show');
 document.querySelector('.modal-wrapper:not(.success-modal__wrapper)').onclick = event => {
     if (event.target.classList.contains('modal-wrapper')) event.target.classList.remove('show')
 }
 document.querySelector('.form-modal__close-button').onclick = () => document.querySelector('.form-modal__wrapper').classList.remove('show');
-
 
 document.querySelector('.open-success-modal-button').onclick = () => {
     document.querySelector('.success-modal__wrapper').classList.add('show');
