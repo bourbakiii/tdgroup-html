fbac7ef128840450982d434811528ccfda5598b9
bourbakiii <gabisov03912@gmail.com>
2022-11-09 15:30:23 +0300

    Control point

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,15 @@ import './sliders/_articles.js'
 import './sliders/_workers.js'
 import './sliders/_cases.js'
 import './sliders/_cases-detail.js'
+
+document.querySelector('.open-form-modal-button').onclick = () => document.querySelector('.form-modal__wrapper').classList.toggle('show');
+document.querySelector('.modal-wrapper:not(.success-modal__wrapper)').onclick = event => {
+    if (event.target.classList.contains('modal-wrapper')) event.target.classList.remove('show')
+}
+document.querySelector('.form-modal__close-button').onclick = () => document.querySelector('.form-modal__wrapper').classList.remove('show');
+
+
+document.querySelector('.open-success-modal-button').onclick = () => {
+    document.querySelector('.success-modal__wrapper').classList.add('show');
+    setTimeout(() => document.querySelector('.success-modal__wrapper').classList.remove('show'), 3000)
+}
